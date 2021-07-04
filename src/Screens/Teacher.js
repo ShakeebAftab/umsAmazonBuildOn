62932bdf63a284b6e7059d85c99e0d00d50951f6
ShakeebSheikh <shakeebaftab@gmail.com>
2021-07-04 08:53:27 +0500

    Dashboard Added

--- src/Screens/Teacher.js
+++ src/Screens/Teacher.js
@@ -4,28 +4,30 @@ import { Grid, Box, Container } from '@material-ui/core';
 import Header from '../Components/Header/Header';
 import AttendanceForm from '../Components/TeacherForm/AttendanceForm';
 import MarksForm from '../Components/TeacherForm/MarksForm';
+import Embed from '../Components/Dashboard/Embed';
 
 const Teacher = () => {
     return (
-        <Box overflow='hidden'>
-            <Grid container direction='column' spacing={4}>
-                <Grid item xs={12}>
-                    <Header hidden={true} />
-                </Grid>
-                <Grid container item spacing={2} style={{paddingBottom: '32px'}}>
-                    <Grid item md={6}>
-                        <Container maxWidth="sm">
-                            <AttendanceForm />
-                        </Container>
-                    </Grid>
-                    <Grid item md={6}>
-                        <Container maxWidth="sm">
-                            <MarksForm />
-                        </Container>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Box>
+        // <Box overflow='hidden'>
+        //     <Grid container direction='column' spacing={4}>
+        //         <Grid item xs={12}>
+        //             <Header hidden={true} />
+        //         </Grid>
+        //         <Grid container item spacing={2} style={{paddingBottom: '32px'}}>
+        //             <Grid item md={6}>
+        //                 <Container maxWidth="sm">
+        //                     <AttendanceForm />
+        //                 </Container>
+        //             </Grid>
+        //             <Grid item md={6}>
+        //                 <Container maxWidth="sm">
+        //                     <MarksForm />
+        //                 </Container>
+        //             </Grid>
+        //         </Grid>
+        //     </Grid>
+        // </Box>
+        <Embed />
     )
 }
 
